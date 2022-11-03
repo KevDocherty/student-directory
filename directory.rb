d63b94214a5ec67ce3db6aeccf7f9e2256880ea4
Kev Docherty <kevdocherty@gmail.com>
2022-11-03 20:27:21 +0000

    prints students by cohort and singular student

--- directory.rb
+++ directory.rb
@@ -2,16 +2,16 @@
 def students_hard_coded
   students = [
     {name: "Dr. Hannibal Lecter", cohort: :november, hobies: "darts, farting"},
-    {name: "Darth Vader", cohort: :november, hobies:  "darts, farting"},
+    {name: "Darth Vader", cohort: :june, hobies:  "darts, farting"},
     {name: "Nurse Ratched", cohort: :november, hobies:  "darts, farting"},
     {name: "Michael Corleone", cohort: :november, hobies:  "darts, farting"},
-    {name: "Alex DeLarge", cohort: :november, hobies:  "darts, farting"},
+    {name: "Alex DeLarge", cohort: :june, hobies:  "darts, farting"},
     {name: "The Wicked Witch of the West", cohort: :november, hobies: "darts, farting"},
     {name: "Terminator", cohort: :november, hobies: "darts, farting"},
     {name: "Freddy Krueger", cohort: :november, hobies: "darts, farting"},
-    {name: "The Joker", cohort: :november, hobies:  "darts, farting"},
+    {name: "The Joker", cohort: :june, hobies:  "darts, farting"},
     {name: "Joffrey Baratheon", cohort: :november, hobies:  "darts, farting"},
-    {name: "Norman Bates", cohort: :november, hobies:  "darts, farting"}
+    {name: "Norman Bates", cohort: :june, hobies:  "darts, farting"}
   ]
   puts "Here are our students: "
   puts students
@@ -36,7 +36,11 @@ def input_students
     end
     # add the student hash to the array
     students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    if students.length == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
   end
   return students
 end
@@ -88,6 +92,7 @@ def print(students)
   # index = 0
   # until index == students.length
     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(75)
+    puts "\n"
     # student_details = "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort) - hobies: #{students[index][:hobies]}"
     #student_details = "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)}"
     # puts "#{index+1}. #{students[index][:name]} (#{students[index][:cohort]} cohort) - hobies: #{students[index][:hobies]}"
@@ -96,14 +101,64 @@ def print(students)
   end
 end
 
+def print_students_by_cohort(students)
+  cohorts = []
+  students.each do |student|
+  
+    cohort = student[:cohort].to_s
+    #puts cohort
+    #puts cohort.class
+    if !cohorts.include? cohort
+      cohorts << cohort
+    end
+  end
+#puts cohorts
+#puts cohorts.length
+
+  cohorts.each do |cohort|
+    puts "This is the #{cohort} cohort:".center(75)
+    puts "-----------------------------".center(75)
+    students.each do |student|
+    #cohort_sym = cohort.to_sym
+      if student[:cohort].to_s == cohort
+        puts student[:name].center(75)
+      end
+    end
+    puts "\n"
+  end
+end
+  
+
 def print_footer(students)
   puts "\n"
   puts "Overall, we have #{students.count} great students!".center(75)
 end
 
-students = students_hard_coded
-#students = input_students
+#students = students_hard_coded
+students = input_students
 student_subset = subset_students(students)
 print(student_subset)
+print_students_by_cohort(student_subset)
+
 print_footer(student_subset)
 
+
+
+  #student.each do |key, value|
+    
+    #name = 
+    #if key == ':cohort'
+    #   puts student[key]
+    #end
+    #puts key
+    #puts value
+    #end
+  '''for i in 1..word.length
+    # puts word[i-1]
+    letter = word[i - 1]
+    if letter == " " 
+      score += 0
+    elsif value.include? letter
+      score += key
+    end'''
+  #end
